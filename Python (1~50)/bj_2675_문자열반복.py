9f2fbc8ff96af10e7cf97c6b69e01f8a75686633
Hyeseung Lee <98807743+hyeseunng@users.noreply.github.com>
2024-04-06 16:20:32 +0900

    Update bj_2675_문자열반복.py
    주석 추가

--- Python (1~50)/bj_2675_문자열반복.py
+++ Python (1~50)/bj_2675_문자열반복.py
@@ -1,13 +1,10 @@
 T = int(input())
 
-# for _ in range(len(T)):  # 반복횟수 # 문자열
-#     R, S = map(input().split())
-#     result = []
-#     for i in range(len(S)):
-#         result = S[i]*R
+for _ in range(T):
+    R, S = input().split()  
+    R = int(R)           # R을 정수로 변환
+    for char in S:        # S의 각 문자에 대해
+        print(char*R,end="")  # 해당 문자를 R번 반복
+    print()               # 다음 입력을 위해 새 줄로 넘어감
 
-for j in range(T):
-    i, S = input().split()
-    for k in S:
-        print(k*int(i),end="")
-    print()
+# 각 출력이 같은 줄에 이어서 나타나게 하고 싶다면 end=""를 사용
